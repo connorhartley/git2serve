9d71c9a18a8bb0bfa85a2824c70031283916c3a2
Connor Hartley <vectrixu+gh@gmail.com>
2018-02-21 20:17:25 +1300

    Try this

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ echo "Cloned ($WEBSITE_REPO) into ($WEBSITE_ID)"
 cd $WEBSITE_ID
 
 # Support pac production deployment (https://www.npmjs.com/package/pac)
-if [ -d ".modules" ]; then
+if [ -d ".modules/" ]; then
   mkdir -p node_modules > /dev/null
   for f in .modules/*.tgz; do tar -zxf "$f" -C node_modules/ > /dev/null; done
   npm rebuild > /dev/null
@@ -49,8 +49,8 @@ fi
 npm start
 
 # Move custom h2o configuration (https://github.com/h2o/h2o/wiki#configuration-examples)
-if [ -d ".h2o" ]; then
-  sudo cp .h2o/h2o.conf /etc/h2o
+if [ -d ".h2o/" ]; then
+  sudo cp .h2o/h2o.conf /etc/h2o/h2o.conf
 fi
 
 echo "Installed ($WEBSITE_ID)"
